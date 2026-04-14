a0605943c8332da9af7acecc3270f9980d5f3b5b
Syeda <clwr0502@leeds.ac.uk>
2026-04-14 17:29:49 +0000

    working on the pseudo code for cal2

--- calculator2.c
+++ calculator2.c
@@ -1,26 +1,18 @@
 //ask user for the input
 // ex:22-3+(4*2)/6
 // ex:22-6/2+(4*2)/6
-// we process the expression lest to right, one char at a time fgets
-// we start with , temp_result=0, current_op ='+', current_num=0
-/* -if sapce skip
-    - if number = build the current_num
-    -if operation 
-        -temp_result = current_number current_op temp_result
-        -store the exp[i] in current_op
-        -current_num=0
-    -if '('
-        -if number = build the current_num
-        -if operation
-            -temp_result = current_number current_op temp_result
-            -store the exp[i] in current_op
-            -current_num=0
-            -return double temp_result
-            if ) -return 0
-    current_number= result from function '('
-    goal result=temp result
+// here we will declare functions before defining it, reason: we have to call it inside other functions that should come first
+/*
+Function 1: skip spaces()
+Function 2:read_parts()= if ( then i++ and use the fuction calculation  
+                        if ) then i++
+                        else print Missing brakets
+                        return 0;
+Function 3: mul_div_calculation()= 
+Function 4: calculation()= if isdigit(exp[i])
 
 */
+
 # include <stdio.h>
 int main()
 {
